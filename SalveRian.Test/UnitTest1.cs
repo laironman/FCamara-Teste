db53359f653cdee9d8c9d8a158c3d71227133558
Luciano Lopes de Azevedo <llopesa@gmail.com>
2018-07-22 21:04:12 -0300

    Estrutura Inicial para Salve Rian

---
+++ SalveRian.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+
+
+using NUnit.Framework;
+
+namespace SalveRian.Test
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ SalveRian/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SalveRian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
